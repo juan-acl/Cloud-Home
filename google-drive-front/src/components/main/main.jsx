2facbb9192ffebad2df7308b2dc2d39cae86e83e
develop <dev-juan-eloper@gmail.com>
2023-02-17 19:33:38 -0600

    Cloud home V1

--- google-drive-front/src/components/main/main.jsx
+++ google-drive-front/src/components/main/main.jsx
@@ -1,66 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Files } from "../files/files";
 import {NavBar} from '../navbar/navbar'
 import {url} from '../url';
 import axios from 'axios';
-import { Archive } from "@mui/icons-material";
-
-
+import  Form  from "react-bootstrap/Form";
+import Button  from "react-bootstrap/Button";
 
 
 function Main() {
 
-
+  const [fls, setFls] = useState([]);
   const [filesupload , setFilesupload] = useState([])
 
+  useEffect(() => {
+    axios.get(url + 'file/files')
+    .then((res) => {
+          console.log(res.data)
+        })
+    .catch((err) => {
+        console.log('No vienen los archivos', err)
+      })
+  }, [filesupload])
+
+  
+
   const handleChange = (e) => {
-    const file = e.target.files[0]
-    file.isUpLoading = true;
-    setFilesupload([...filesupload, file])
+    const file = e.target.files 
+    setFilesupload(file)
   }
 
   const cargarArchivos = (e) => {
     e.preventDefault();
-    axios.post(url + 'file/upload', filesupload)
+    const data = new FormData();
+    for(const fl of filesupload) {
+      data.append('files', fl)
+    }
+   axios.post(url + 'file/upload', data)
     .then((res) => {
       console.log('archivos cargados')
     })
     .catch((err) => {
-      console.log(err)
+      console.log(err, 'NO se pudo cargar')
     })
+    
   }
 
 
 return (
-  <div>
-  <NavBar />
-<div >
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <label> Seleccione sus archivos: </label>
-        <input name="files" id='files' onChange={handleChange} type='file' placeholder="browser" />
+  <>
+      <NavBar />
+      <Files />
 
-  </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary" onClick={cargarArchivos} >Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
-</div>
-  <Files />
-  </div>
+  <Form className="mb-3" onSubmit={cargarArchivos} >
+    <Form.Label> Upload files </Form.Label>
+    <Form.Group>
+          <Form.Control onChange={handleChange} type='file' multiple />
+    </Form.Group>
+    <Button variant="primary" type="submit" >
+    Upload files
+  </Button>
+  </Form>
+</> 
+  
 )
 }
 
