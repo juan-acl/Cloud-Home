ea92fe258847332202882d99ec5ce14653f8097e
develop <dev-juan-eloper@gmail.com>
2023-02-21 11:01:42 -0600

    Refractor FileList

--- google-drive-front/src/components/files/files.jsx
+++ google-drive-front/src/components/files/files.jsx
@@ -1,38 +1,77 @@
 import React, { useEffect, useState } from 'react';
 import {url} from '../url'
-import Axios from 'axios';
+import axios from 'axios';
 import { File } from '../file/file';
 import './files.css'
-
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import Swal from 'sweetalert2';
 
 function Files () {
-  
 
-  const [files, setFiles] = useState([])
-  
+  const [files, setFiles] = useState([]);
+  const [filesupload, setFilesupload] = useState([]);
+  const [open, setOpen] = useState(false)
 
-  const getFiles = () => {
-    Axios.get(url + 'file/files')
-    .then((res) => {
-      setFiles(res.data.files)
-    })
-    .catch((err) => {
-      console.log(err)
-    })}
+  const fetchData = async () => {
+  const response = await axios.get(url + 'file/files')
+  setFiles(response.data.files)
+    }
 
-  useEffect (() => {
-    getFiles();
+useEffect(() => {
+    fetchData()
   }, []);
 
+  const handleChange = (e) => {
+    const file = e.target.files
+    setFilesupload(file)
+    setOpen(false)
+  }
+
+  const submitFiles = async (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    for(const fl of filesupload) {
+      data.append('files', fl)
+    }
+    axios.post(url + 'file/upload', data)
+    .then((res) => {
+        Swal.fire({
+          position: 'center',
+          icon: 'success',
+          color: 'white',
+          background: 'rgba(0,0,0,0.9)',
+          title: res.data.Message,
+          timer: 1000,
+          toast: true,
+          showCancelButton: false,
+          showConfirmButton: false
+        })
+        const newFile = res.data.files
+        setFilesupload(newFile)
+        e.target.reset()
+        setOpen(prevState => !prevState)
+        fetchData()
+      })
+    .catch((err) => {
+        console.log(err)
+      })
+  }
 
 
-      return (
-<div>
-<div>
+return (
 <div>
     <div className='container' >
       <div className='row' >
-              {!files.length && <h1 className='no-content' >No content files</h1> }
+          <div>
+            <Form onSubmit={submitFiles} className='mb-3' >
+              <Form.Group>
+                <Form.Control onChange={handleChange} style={{marginTop: 20}} type='file' multiple />
+              </Form.Group>
+              {!filesupload.length ? <h4 style={{marginTop: 10, marginLeft: 10}}> Seleccione sus archivos para subir </h4> : <Button style={{marginTop: 10}} disabled={open} variant='primary' type='submit' > Upload files </Button> }
+            </Form>
+          </div>
+          {!files.length && <h1 style={{textAlign: 'center'}} >NO CONTENT FILES</h1>}
         {
           files.map((item, index) => {
             return (
@@ -43,8 +82,6 @@ function Files () {
           })
         }
       </div>
-    </div>
-    </div>
     </div>
     </div>
   )
--- google-drive-front/src/components/main/main.jsx
+++ google-drive-front/src/components/main/main.jsx
@@ -1,86 +1,17 @@
-import React, { useEffect, useState, useReducer } from "react";
+import React from 'react'
 import { Files } from "../files/files";
 import {NavBar} from '../navbar/navbar'
-import {url} from '../url';
-import axios from 'axios';
-import  Form  from "react-bootstrap/Form";
-import Button  from "react-bootstrap/Button";
 
 
 function Main() {
 
-  const [filesupload , setFilesupload] = useState([])
-  const [axiosGet, setAxiosGet] = useState([])
-
-
-    useEffect(() => {
-    const fetchData = async () => {
-      const data = axios.get(url + 'file/files')
-      .then((res) => {
-          setFilesupload(res.data.files)
-        })
-      .catch((err) => {
-          console.log(err)
-        })
-    }
-    fetchData();
-  }, [])
-
-  const handleChange = (e) => {
-    const file = e.target.files 
-    setFilesupload(file)
-  }
-
-    
-  const getFiles = (e) => {
-  axios.get(url + 'file/files')
-    .then((res) => {
-        setAxiosGet(res.data.files)
-      console.log('desde el main',res.data)
-      })
-    .catch((err) => {
-        console.log(err, 'No vienen los archivos')
-      })
-  } 
-
-  const cargarArchivos = async (e) => {
-    e.preventDefault();
-    const data = new FormData();
-    for(const fl of filesupload) {
-      data.append('files', fl)
-    }
-   const result = await axios.post(url + 'file/upload', data)
-    .then((res) => {
-        setFilesupload(res.data.files)
-    })
-    .catch((err) => {
-      console.log(err, 'NO se pudo cargar')
-    })
-    
-  }
-
-
-
-
-
 
 return (
   <>
-      <NavBar />
-      <Files/>
-
-  <Form className="mb-3" onSubmit={cargarArchivos} >
-    <Form.Label> Upload files </Form.Label>
-    <Form.Group>
-          <Form.Control onChange={handleChange} type='file' multiple />
-    </Form.Group>
-    <Button variant="primary" type="submit" >
-    Upload files
-  </Button>
-  </Form>
-</> 
-  
-)
+    <NavBar />
+    <Files/>
+  </> 
+ )
 }
 
 export {Main};
